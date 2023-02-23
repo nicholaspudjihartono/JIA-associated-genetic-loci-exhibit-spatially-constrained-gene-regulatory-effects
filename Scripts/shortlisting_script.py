dd0de3aa50c8a3fa130f9659f7c97ac75274c7e4
nicholasm96 <91126126+nicholasm96@users.noreply.github.com>
2023-02-23 19:30:08 +1300

    Update shortlisting_script.py
    Final version.
    This script outputs two files : "significant_eqtls_shortlisted.txt"
    and
    "eqtls_shortlisted.txt"

--- Scripts/shortlisting_script.py
+++ Scripts/shortlisting_script.py
@@ -6,8 +6,7 @@ import statsmodels
 from statsmodels.stats.multitest import fdrcorrection
 pandas.options.mode.chained_assignment = None
 
-#"risk_loci_dict" is a dictionary containing 71 JIA risk loci as the keys, and its LD partners as the values (see Supplementary Table 3 in the manuscript)
-
+#"risk_loci_dict" below is a dictionary containing 71 JIA risk loci as the keys, and its LD partners as the values (see Supplementary Table 3 in the manuscript)
 risk_loci_dict = {'rs6679677': ['rs6679677'],
  'rs72698115': ['rs73026617', 'rs111810442', 'rs72698115'],
  'rs4648881': ['rs10903110',
@@ -641,7 +640,6 @@ risk_loci_dict = {'rs6679677': ['rs6679677'],
 
 # The function below is to categorize each SNP by the risk locus it belongs to
 # For example, "categorize_by_risk_locus('rs2549782')" will output "rs4869313/rs4869314" as the risk locus
-
 def categorize_by_risk_locus(SNP):
     for risk_locus,LD_partners in risk_loci_dict.items():
         if SNP in LD_partners:
@@ -650,7 +648,6 @@ def categorize_by_risk_locus(SNP):
      
      
 #Create the argument parser that takes in the CoDeS3D output files 'eqtls.txt' , 'snps.txt', and 'genes.txt'
-
 parser = argparse.ArgumentParser(description='')
 parser.add_argument("-e","--eqtls", type=argparse.FileType('r'), required=True, metavar = "eqtls.txt", help = "A tab-separated file containing eQTL-target gene pairs and its corresponding P-values, this is the CoDeS3D 'eqtls.txt' output file", dest = "eqtls")
 parser.add_argument("-g", "--genes", type=argparse.FileType('r'), required=True, metavar="genes.txt", help = "A tab-separated file showing all the genes that are physically interacting with the input SNPs, this is the CoDeS3D 'genes.txt' output file", dest = "genes")
@@ -658,7 +655,6 @@ parser.add_argument("-s", "--snps", type=argparse.FileType('r'), required=True, 
 args = parser.parse_args()
 
 # Read the input TSV files into a pandas DataFrame
-
 eqtls = pandas.read_csv(args.eqtls, sep = '\t')
 genes = pandas.read_csv(args.genes, sep = '\t')
 snps = pandas.read_csv(args.snps, sep = '\t')
@@ -666,12 +662,10 @@ snps = pandas.read_csv(args.snps, sep = '\t')
 
 #At the current state, the "eqtls.txt" file does not contain rsID information for each SNPs, it only contain the variant ID. 
 #In order to get the rsID information, we merge "eqtls.txt" with the ['snp','variant_id'] columns of "snps.txt". the ['snp'] column contains the rsID information. 
-
 eqtls =  eqtls.merge(snps[['snp','variant_id']].drop_duplicates(),how='left',left_on='sid',right_on='variant_id')
 
 #At the current state, the "eqtls.txt" file does not contain gene name information for each target gene, it only contain the the gene GENCODE ID. 
 #In order to get the gene name information, we merge "eqtls.txt" with the ['gene','gencode_id'] columns of "genes.txt". the ['gene'] column contains the gene name information. 
-
 eqtls = eqtls.merge(genes[['gene','gencode_id']].drop_duplicates(),how='left', left_on='pid', right_on='gencode_id')
 
 #Remove redundant columns from 'eqtls' dataframe
@@ -683,7 +677,6 @@ eqtls = eqtls.drop(columns=['sid','pid'])
 
      
 #Now we create a new column called "risk_locus" which will contain the risk locus information of each SNP in the dataframe !
-
 eqtls['risk_locus'] = ''
 eqtls['risk_locus'] = eqtls['snp'].map(categorize_by_risk_locus)
 
@@ -691,11 +684,9 @@ eqtls['risk_locus'] = eqtls['snp'].map(categorize_by_risk_locus)
 
 #Now, we need to select the spatial eQTL with the lowest p-value for each risk locus-gene-tissue combinations as a representative of the risk locus' regulatory effect on a particular gene in a specific tissue/immune cell type prior to FDR correction.
 #The rationale behind this approach is explained in Supplementary Figure 3 in the manuscript
-
 eqtls_minimumP = eqtls.loc[eqtls.groupby(['tissue','risk_locus','gene'])['pval'].idxmin()]
 
 #Save this "eqtls_minimumP" as a csv file called "eqtls_shortlisted.txt". But we want to follow the format of the CoDeS3D output file "eqtls.txt"
-
 eqtls_shortlisted = eqtls_minimumP[['variant_id','gencode_id','sid_chr','sid_pos','adj_pval','pval','b','b_se','maf','tissue']]
 eqtls_shortlisted.rename(columns = {'variant_id':'sid','gencode_id':'pid'}, inplace = True)
 eqtls_shortlisted.to_csv('eqtls_shortlisted.txt',sep='\t', index=False)
@@ -707,7 +698,6 @@ eqtls_shortlisted.to_csv('eqtls_shortlisted.txt',sep='\t', index=False)
 eqtls_minimumP['adj_pval_shortlisted'] = ''
 
 #Lets now fill the ['adj_pval_shortlisted'] column with the appropriate value
-
 for x in list(eqtls_minimumP['tissue'].unique()):
     subset = eqtls_minimumP[eqtls_minimumP['tissue'] == x]
     fdr = fdrcorrection(subset['pval'])
@@ -719,7 +709,6 @@ significant_eqtls_shortlisted = eqtls_minimumP[eqtls_minimumP['adj_pval_shortlis
 
 
 #Re-order the columns for easier reading
-
 significant_eqtls_shortlisted = significant_eqtls_shortlisted.reindex(columns=['risk_locus','variant_id','snp','sid_chr','sid_pos','gencode_id','gene','adj_pval_shortlisted','adj_pval','pval','b','b_se','maf','tissue'])
 
 #Save as a csv file
